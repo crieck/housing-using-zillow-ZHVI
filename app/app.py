7f606537c0544dd835f54262b5d55113e6c5d989
Chris Rieck <crieck@gmail.com>
2019-03-26 19:32:20 -0500

    updated template rendering and added bootstrap

--- app/app.py
+++ app/app.py
@@ -1,14 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 
+
+#Application routes
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/index')
+def index():
+    return render_template('home.html')
+
+@app.route('/charts')
+def charts():
+    return render_template('charts.html')
+
+@app.route('/map')
+def map():
+    return render_template('map.html')
 
 @app.route('/about')
 def about():
-    return 'The about page'
+    return render_template('about.html')
 
-@app.route('/dashboard/')
-def projects():
-    return 'The dashboard page'
+if __name__ == '__main__':
+    app.run(debug=True)
